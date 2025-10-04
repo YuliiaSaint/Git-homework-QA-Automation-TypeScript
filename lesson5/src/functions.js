cb5907228b7d6fdae1054f5b12ff696271395e74
Yuliia <sviatenkayuliia@gmail.com>
2025-10-04 22:55:43 +0200

    Refactor: додано універсальну функцію reduce у functions.js

--- lesson5/src/functions.js
+++ lesson5/src/functions.js
@@ -12,6 +12,14 @@ function joinStrings(strings) {
   return strings.join(" "); // об’єднання через пробіл
 }
 
+// універсальна функція для чисел і рядків (через reduce)
+function sumElements(arr) {
+  return arr.reduce(
+    (sum, num) => sum + num,
+    typeof arr[0] === "string" ? "" : 0
+  );
+}
+
 // Масив чисел (повтори в залі)
 const reps = [10, 12, 15];
 
@@ -19,5 +27,9 @@ const reps = [10, 12, 15];
 const exercises = ["squats", "push-ups", "plank"];
 
 // Виклик функцій
-console.log("Загальна кількість повторів:", sumNumbers(reps)); 
+console.log("Загальна кількість повторів:", sumNumbers(reps));
 console.log("Список вправ:", joinStrings(exercises));
+
+// Виклик універсальної функції
+console.log("Універсальна функція (числа):", sumElements(reps));
+console.log("Універсальна функція (рядки):", sumElements(exercises));
