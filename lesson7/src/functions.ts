35b089fc712bed543c906cdb9d3002a05c550cdc
Yuliia <sviatenkayuliia@gmail.com>
2025-10-11 12:08:06 +0200

    Fix sumElemets by splittinhg into sumNumbers and joinSrring

--- lesson7/src/functions.ts
+++ lesson7/src/functions.ts
@@ -1,19 +1,19 @@
-// Універсальна функція для масивів чисел або рядків
-export function sumElements(arr: (number | string)[]): number | string {
-  return arr.reduce(
-    (sum, item) =>
-      typeof item === "number"
-        ? (sum as number) + item
-        : (sum as string) + item,
-    typeof arr[0] === 'string' ? "" : 0
-  );
+//Функція для масиву чисел
+export function sumNumbers(arr: number[]): number {
+  return arr.reduce((sum, num) => sum + num, 0);
 }
 
-// Масив чисел
+//Функція для масиву рядків
+export function joinStrings(arr: string[]): string {
+  return arr.join("");
+}
+
+//Масив чисел
 const reps: number[] = [10, 12, 15];
 
-// Масив рядків
+//Масив рядків
 const exercises: string[] = ["squats", "push-ups", "plank"];
 
-console.log("Сума чисел:", sumElements(reps)); // 37
-console.log("Об'єднані рядки:", sumElements(exercises)); // "squatspush-upsplank"
+//Виклики функцій
+console.log("Сума чисел:", sumNumbers(reps)); // 37
+console.log("Об'єднані рядки:", joinStrings(exercises)); // "squatspush-upsplank"
